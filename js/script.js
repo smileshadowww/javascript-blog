01ad7387e6b1d58d704cea841f9a7767125d86ba
Rafał Stekla <steklarafal13@gmail.com>
2021-12-06 21:18:18 +0100

    5.3

--- js/script.js
+++ js/script.js
@@ -1,28 +1,37 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
+  const clickedElement = this;
   console.log('Link was clicked!')
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  this.classList.add('active');
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targerArticle = document.querySelector(articleSelector);
+  console.log(targerArticle);
 
   /* add class 'active' to the correct article */
+  targerArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
